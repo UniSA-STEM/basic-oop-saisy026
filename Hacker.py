04a7723c2baee5d1a22ec901a78e418b5ec09c9b
saisy026 <saisy026@mymail.unisa.edu.au>
2025-10-19 14:19:22 +1030

    exposed added which helps to check if the hacker's trace level is high or not.

--- Hacker.py
+++ Hacker.py
@@ -6,13 +6,15 @@ ID: 110434667
 Username: saisy026
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
-
+from Asset import Asset, CryptoToken, DataSpike, RemovableDrive, SecurityChip, HardwarePatch
+from Rig import Rig
 class Hacker:
     def __init__(self,name):
         self.name = name
         self.trace_level = 0
         self.inventory = ['CryptoToken']
         self.rig = None
+        self.threshold = 5
 
     def acquire_rig(self,rig):
         for item in self.inventory:
@@ -22,4 +24,9 @@ class Hacker:
                 print(f"{self.name} has acquired rig '{rig.name}'.")
                 return
         print("No CryptoToken available to acquire rig.")
+
+    def is_exposed(self):
+        return self.trace_level > self.threshold
+
+
 
